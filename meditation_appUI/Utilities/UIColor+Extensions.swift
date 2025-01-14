c2c027c6191470d5d9318adccce264059f810d0a
cao đức tín <102477829+caoductin@users.noreply.github.com>
2025-01-14 12:28:35 +0700

    Config App Theme

--- meditation_appUI/Utilities/UIColor+Extensions.swift
+++ meditation_appUI/Utilities/UIColor+Extensions.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIColor {
+
+    convenience init?(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        // Ensure the string starts with "#" or is valid
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted.remove(at: hexFormatted.startIndex)
+        }
+        
+        // The hex string should now be 6 or 8 characters
+        guard hexFormatted.count == 6 else { return nil }
+        
+        // Convert hex string to an integer
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        // Extract RGB components
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
+        
+        // Initialize UIColor with RGB and alpha
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
